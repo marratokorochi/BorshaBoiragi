12e86f04da3bd31dbcba09892138edd179a80bfb
Orochi_Boss <120943220+marratokorochi@users.noreply.github.com>
2023-01-12 18:18:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName: Borsha");
+ var b=prompt("Enter password: 22 December");
+  	if(a== "Borsha" && b=="22 December")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
